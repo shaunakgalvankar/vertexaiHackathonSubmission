bc9b55c3b593fc7767a47f76b8ef3171aa35f24d
Shaunak <shaunakprasad@gmail.com>
2023-07-07 18:01:27 -0700

    added the video and the chat and sidebar component

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-import streamlit as sl
+import streamlit as st
+
+
+st.title('SpyrAI')
+
+video_file = open('../demo.mov', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+# Object notation
+sidebar = st.sidebar.selectbox(
+    "Videos",
+    (video_file)
+)
+prompt = st.text_input("Got Questions?")
+
 
