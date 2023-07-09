ef894083f99ba19e1d642c57fe9584a54583e9dd
Shaunak <shaunakprasad@gmail.com>
2023-07-08 17:12:33 -0700

    google cloud init

--- azure.py
+++ azure.py
@@ -1,28 +1,69 @@
-# import streamlit as st
-# from azure.identity import DefaultAzureCredential
-# from azure.media.services.aio import AzureMediaServices
-# from azure.media.streaming.aio import StreamingEndpoint
+import streamlit as st
+import os
+from langchain.llms import VertexAI
+from langchain import PromptTemplate, LLMChain
+from langchain.document_loaders import YoutubeLoader
 
-# # Authenticate with Azure
-# credential = DefaultAzureCredential()
-# ams_client = AzureMediaServices(credential)
+from google.cloud import aiplatform
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from google.cloud import aiplatform
 
-# # Create a streaming endpoint
-# endpoint_name = "my-streaming-endpoint"
-# endpoint_location = "westus"
-# streaming_endpoint = await ams_client.streaming_endpoints.create_or_update(
-#     resource_group_name="<your-resource-group-name>",
-#     account_name="<your-azure-media-services-account-name>",
-#     streaming_endpoint_name=endpoint_name,
-#     parameters=StreamingEndpoint(
-#         location=endpoint_location,
-#         ...  # Add any other necessary parameters
-#     ),
-# )
+aiplatform.init(
+    # your Google Cloud Project ID or number
+    # environment default used is not set
+    project='spyrai',
 
-# # Start the streaming endpoint
-# await streaming_endpoint.start()
+    # the Vertex AI region you will use
+    # defaults to us-central1
+    location='us-central1',
 
-# # Display the streaming endpoint information
-# st.write(f"Streaming Endpoint Name: {streaming_endpoint.name}")
-# st.write(f"Streaming Endpoint URL: {streaming_endpoint.host_name}")
+    # Google Cloud Storage bucket in same region as location
+    # used to stage artifacts
+    staging_bucket='gs://my_staging_bucket',
+
+    # custom google.auth.credentials.Credentials
+    # environment default creds used if not set
+    credentials="AIzaSyAf9YstQtzv5TC0VpoJ5-_2hOoq-En2vxM",
+
+    # customer managed encryption key resource name
+    # will be applied to all Vertex AI resources if set
+    # encryption_spec_key_name=my_encryption_key_name,
+
+    # the name of the experiment to use to track
+    # logged metrics and parameters
+    experiment='my-experiment',
+
+    # description of the experiment above
+    experiment_description='my experiment decsription'
+)
+
+video_url="https://www.youtube.com/watch?v=jRAAaDll34Q&ab_channel=CoreySchafer"
+
+loader = YoutubeLoader.from_youtube_url(
+    video_url, add_video_info=True
+)
+
+docs=loader.load()
+transcribedText=docs[0].page_content
+
+API_KEY=AIzaSyAf9YstQtzv5TC0VpoJ5-_2hOoq-En2vxM
+
+#split a transcript
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
+splits = text_splitter.split_text(transcribedText)
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+llm = VertexAI()
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+question = "What NFL team won the Super Bowl in the year Justin Beiber was born?"
+
+response=llm_chain.run(question)
+
+print(response)
